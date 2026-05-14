a82a0ededa1a8bc05319fcaf9f803a1112bce71d
Clowsiax <akincibatuhann@gmail.com>
2026-05-14 13:27:28 +0300

    not_sistemi.py

--- ogrenci_not_takip/not_sistemi.py
+++ ogrenci_not_takip/not_sistemi.py
@@ -8,26 +8,22 @@ class NotSistemi:
     def ogrenci_ekle(self, ad, soyad, numara):
         ogrenci = Ogrenci(ad, soyad, numara)
         self.ogrenciler.append(ogrenci)
-        print("\nÖğrenci başarıyla eklendi.")
 
     def ogrenci_bul(self, numara):
         for ogrenci in self.ogrenciler:
             if ogrenci.numara == numara:
                 return ogrenci
+
         return None
 
     def not_gir(self, numara, vize, final):
         ogrenci = self.ogrenci_bul(numara)
 
-        if ogrenci is None:
-            print("\nÖğrenci bulunamadı.")
-        else:
+        if ogrenci:
             ogrenci.not_gir(vize, final)
-            print("\nNotlar başarıyla kaydedildi.")
+            return True
 
-    def ogrencileri_listele(self):
-        if len(self.ogrenciler) == 0:
-            print("\nSistemde kayıtlı öğrenci yok.")
-        else:
-            for ogrenci in self.ogrenciler:
-                ogrenci.bilgi_goster()
+        return False
+
+    def ogrencileri_getir(self):
+        return self.ogrenciler
